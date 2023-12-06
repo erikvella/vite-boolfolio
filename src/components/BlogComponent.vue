f78bdeaac29576e65207d1308c04a54f947b9026
erikvella <vella.erik1@gmail.com>
2023-12-06 21:52:52 +0100

    [add] cards

--- src/components/BlogComponent.vue
+++ src/components/BlogComponent.vue
@@ -21,14 +21,20 @@ export default {
 <template>
   <div>
     <h1>I miei progetti!!!</h1>
-    <ul>
-     <li v-for="project in store.projects" :key="project.id">
-          <p>{{ project.title }}</p>
-     </li>
-    </ul>
+    
+    <div class="card" v-for="project in store.projects" :key="project.id">
+      <h3>Nome del progetto: {{ project.title }}</h3>
+      <em>Tipologia: {{ project.type.name }}</em>
+      <h4>Tecnologie usate:</h4>
+      <h4 v-for="tecnology in project.tecnologies" :key="tecnology.id"> {{ tecnology.name }}</h4>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+.card{
+  background-color: bisque;
+  color: black;
+  border: 1px solid rgb(249, 249, 249);
+}
 </style>
