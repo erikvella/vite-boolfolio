435b5b75379b20deecb893718201169798131738
erikvella <vella.erik1@gmail.com>
2023-12-06 17:21:47 +0100

    [add] get Api

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script>
+import axios from 'axios';
+import {store} from './data/store';
 
+export default{
+  name : 'App',
+  data(){
+    return{
+      titolo : 'Bentornati su vue'
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(store.apiUrl + 'projects')
+      .then(results =>{
+        console.log(results.data);
+      })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
+}
 </script>
 
 <template>
-  <div>
-    <h1>ariciao vue</h1>
+  <div class="container">
+    <h1>{{ titolo }}</h1>
   </div>
 </template>
 
